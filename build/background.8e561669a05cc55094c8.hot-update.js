d6335b98988dc9d6354b8c6bf49381323079f5f4
Raul M <118123543+pillow3@users.noreply.github.com>
2024-04-25 22:50:42 +0100

    first commit

---
+++ build/background.8e561669a05cc55094c8.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdatechrome_extension_boilerplate_react"]("background",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("f630183a75e536714b14")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=background.8e561669a05cc55094c8.hot-update.js.map
